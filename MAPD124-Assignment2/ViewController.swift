004e2cbdbe045d90ccb38c1fae18450942fd236a
shelalaine <schan211@my.centennialcollege.ca>
2017-01-31 21:06:18 -0500

    Initial Commit

---
+++ MAPD124-Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD124-Assignment2
+//
+//  Created by Shelalaine Chan on 2017-01-31.
+//  Copyright © 2017 ShelalaineChan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
