579992a39457e4dcc12896d9a92b7981e3f09bc9
shelalaine <schan211@my.centennialcollege.ca>
2017-01-31 21:53:22 -0500

    Add table view for the tasks Add TodoTableViewCell file

---
+++ MAPD124-Assignment2/TodoTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TodoTableViewCell.swift
+//  MAPD124-Assignment2
+//
+//  Created by Shelalaine Chan on 2017-01-31.
+//  Copyright © 2017 ShelalaineChan. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var taskLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MAPD124-Assignment2/ViewController.swift
+++ MAPD124-Assignment2/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let data: [String] = ["Task 1", "Task 2", "Task 3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "rows", for: indexPath) as! TodoTableViewCell
+        
+        return cell;
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
 
 }
 
